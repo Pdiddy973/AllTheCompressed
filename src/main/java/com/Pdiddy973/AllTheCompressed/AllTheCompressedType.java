40d12ce961345993313f7c3125cfbd11f2b2cfaa
Pdiddy973 <68764896+Pdiddy973@users.noreply.github.com>
2022-01-24 20:54:20 -0500

    Adds Tuff #13

--- src/main/java/com/Pdiddy973/AllTheCompressed/AllTheCompressedType.java
+++ src/main/java/com/Pdiddy973/AllTheCompressed/AllTheCompressedType.java
@@ -96,6 +96,7 @@ public enum AllTheCompressedType {
     SPRUCE_LOG("spruce_log", Spruce_Log::new),
     SPRUCE_PLANK("spruce_plank", Spruce_Plank::new),
     STONE("stone", Stone::new),
+    TUFF("tuff", Tuff::new),
 
     //PB
     Wax("wax", Wax::new);
