2fc6bd02254f75c76d34f4c350dfe6f6af28ad59
Pdiddy973 <68764896+Pdiddy973@users.noreply.github.com>
2022-05-25 04:52:58 -0400

    Peridot,Ruby,Sapphire
    - inital commit of new gems

---
+++ src/main/java/com/Pdiddy973/AllTheCompressed/blocks/ato/Peridot.java
@@ -0,0 +1,15 @@
+package com.Pdiddy973.AllTheCompressed.blocks.ato;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.Material;
+
+
+public class Peridot extends Block {
+    public Peridot() {
+        super(Properties.of(Material.METAL)
+            .sound(SoundType.METAL)
+            .requiresCorrectToolForDrops()
+            .strength(7.0f, 20.0f));
+    }
+}
---
+++ src/main/java/com/Pdiddy973/AllTheCompressed/blocks/ato/Sapphire.java
@@ -0,0 +1,14 @@
+package com.Pdiddy973.AllTheCompressed.blocks.ato;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.Material;
+
+public class Sapphire extends Block {
+    public Sapphire() {
+        super(Properties.of(Material.METAL)
+            .sound(SoundType.METAL)
+            .requiresCorrectToolForDrops()
+            .strength(7.0f, 20.0f));
+    }
+}
